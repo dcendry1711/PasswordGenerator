ecca5a481b87733ada34377209867c3d2952b2fa
dcendry1711 <daniel.cendry@gmail.com>
2025-12-09 16:55:38 +0100

    copy passwords to clipboard

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const genBtn = document.getElementById('gen-pass-btn')
 const pass1 = document.getElementById('pass-field1')
 const pass2 = document.getElementById('pass-field2')
 const inputEl = document.getElementById('password-length-input')
+const copy1PassBtn = document.getElementById('copy-pass1')
+const copy2PassBtn = document.getElementById('copy-pass2')
 
 genBtn.addEventListener('click',generatePassword)
 
@@ -25,3 +27,27 @@ function generatePassword(){
     pass1.textContent = password1
     pass2.textContent = password2
 }
+
+copy1PassBtn.addEventListener('click', ()=>{
+    const pass1ToCopy = pass1.textContent
+    navigator.clipboard.writeText(pass1ToCopy)
+        .then(()=>{
+            copy1PassBtn.textContent = 'Copied to clipboard'
+        })
+        .catch(err => {
+            pass1.textContent = 'Error, chceck console'
+            console.log(err)
+        })
+})
+
+copy2PassBtn.addEventListener('click', ()=>{
+    const pass2ToCopy = pass2.textContent
+    navigator.clipboard.writeText(pass2ToCopy)
+        .then(()=>{
+            copy2PassBtn.textContent = 'Copied to clipboard'
+        })
+        .catch(err => {
+            pass2.textContent = 'Error, chceck console'
+            console.log(err)
+        })
+})
