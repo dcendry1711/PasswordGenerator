294d802974e437cd3d1a8e5172c0bb2de83b5f90
dcendry1711 <daniel.cendry@gmail.com>
2025-12-09 16:32:41 +0100

    select length of generated passwords

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ import { characters } from "./characters.js";
 const genBtn = document.getElementById('gen-pass-btn')
 const pass1 = document.getElementById('pass-field1')
 const pass2 = document.getElementById('pass-field2')
+const inputEl = document.getElementById('password-length-input')
 
 genBtn.addEventListener('click',generatePassword)
 
@@ -11,12 +12,12 @@ function generatePassword(){
     let password1 = ''
     let password2 = ''
 
-    for (let i = 0; i < 15; i++){
+    for (let i = 0; i < inputEl.value; i++){
         const randomIndex = Math.floor(Math.random()*characters.length)
         password1 += characters[randomIndex]
     }
 
-    for (let i = 0; i < 15; i++){
+    for (let i = 0; i < inputEl.value; i++){
         const randomIndex = Math.floor(Math.random()*characters.length)
         password2 += characters[randomIndex]
     }
