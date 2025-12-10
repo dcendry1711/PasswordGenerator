86dcf98d42dd5262169e60e6a4ea573af99da4e7
dcendry1711 <daniel.cendry@gmail.com>
2025-12-10 19:34:50 +0100

    improve ux by implement choosing elements in passwords

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { characters } from "./characters.js";
+import { characters,symbols,numbers } from "./characters.js";
 
 const genBtn = document.getElementById('gen-pass-btn')
 const pass1 = document.getElementById('pass-field1')
@@ -6,27 +6,11 @@ const pass2 = document.getElementById('pass-field2')
 const inputEl = document.getElementById('password-length-input')
 const copy1PassBtn = document.getElementById('copy-pass1')
 const copy2PassBtn = document.getElementById('copy-pass2')
+const symbolEl = document.getElementById('password-include-symbols')
+const numberEl = document.getElementById('password-include-numbers')
 
 genBtn.addEventListener('click',generatePassword)
 
-function generatePassword(){
-
-    let password1 = ''
-    let password2 = ''
-
-    for (let i = 0; i < inputEl.value; i++){
-        const randomIndex = Math.floor(Math.random()*characters.length)
-        password1 += characters[randomIndex]
-    }
-
-    for (let i = 0; i < inputEl.value; i++){
-        const randomIndex = Math.floor(Math.random()*characters.length)
-        password2 += characters[randomIndex]
-    }
-
-    pass1.textContent = password1
-    pass2.textContent = password2
-}
 
 copy1PassBtn.addEventListener('click', ()=>{
     const pass1ToCopy = pass1.textContent
@@ -51,3 +35,43 @@ copy2PassBtn.addEventListener('click', ()=>{
             console.log(err)
         })
 })
+
+function getRandomNumber(arr){
+    return Math.floor(Math.random()*arr.length)
+    
+}
+
+function generatePassword(){
+    let password1 = ''
+    let password2 = ''
+    for (let i = 0; i < inputEl.value; i++){
+        pass1.textContent = ''
+        pass2.textContent = ''
+        if(symbolEl.checked && !numberEl.checked){
+            const symbolArr = [...characters,...symbols]
+            const randomIndex1 = getRandomNumber(symbolArr)
+            password1 += symbolArr[randomIndex1]
+            const randomIndex2 = getRandomNumber(symbolArr)
+            password2 += symbolArr[randomIndex2]
+        } else if(numberEl.checked && !symbolEl.checked){
+            const numberArr = [...characters,...numbers]
+            const randomIndex1 = getRandomNumber(numberArr)
+            password1 += numberArr[randomIndex1]
+            const randomIndex2 = getRandomNumber(numberArr)
+            password2 += numberArr[randomIndex2]
+        } else if(numberEl.checked && symbolEl.checked){
+            const fullArr = [...characters,...symbols,...numbers]
+            const randomIndex1 = getRandomNumber(fullArr)
+            password1 += fullArr[randomIndex1]
+            const randomIndex2 = getRandomNumber(fullArr)
+            password2 += fullArr[randomIndex2]
+        } else {
+            const randomIndex1 = getRandomNumber(characters)
+            password1 += characters[randomIndex1]
+            const randomIndex2 = getRandomNumber(characters)
+            password2 += characters[randomIndex2]
+        }
+        pass1.textContent = password1
+        pass2.textContent = password2
+    }
+}
