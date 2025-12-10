8e0222d1858ff0273b5b800253ddf9eb7a8b3270
dcendry1711 <daniel.cendry@gmail.com>
2025-12-10 19:38:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,30 +46,30 @@ function generatePassword(){
     for (let i = 0; i < inputEl.value; i++){
         pass1.textContent = ''
         pass2.textContent = ''
-        if(symbolEl.checked && !numberEl.checked){
-            const symbolArr = [...characters,...symbols]
-            const randomIndex1 = getRandomNumber(symbolArr)
-            password1 += symbolArr[randomIndex1]
-            const randomIndex2 = getRandomNumber(symbolArr)
-            password2 += symbolArr[randomIndex2]
-        } else if(numberEl.checked && !symbolEl.checked){
-            const numberArr = [...characters,...numbers]
-            const randomIndex1 = getRandomNumber(numberArr)
-            password1 += numberArr[randomIndex1]
-            const randomIndex2 = getRandomNumber(numberArr)
-            password2 += numberArr[randomIndex2]
-        } else if(numberEl.checked && symbolEl.checked){
-            const fullArr = [...characters,...symbols,...numbers]
-            const randomIndex1 = getRandomNumber(fullArr)
-            password1 += fullArr[randomIndex1]
-            const randomIndex2 = getRandomNumber(fullArr)
-            password2 += fullArr[randomIndex2]
-        } else {
-            const randomIndex1 = getRandomNumber(characters)
-            password1 += characters[randomIndex1]
-            const randomIndex2 = getRandomNumber(characters)
-            password2 += characters[randomIndex2]
-        }
+            if(symbolEl.checked && !numberEl.checked){
+                const symbolArr = [...characters,...symbols]
+                const randomIndex1 = getRandomNumber(symbolArr)
+                password1 += symbolArr[randomIndex1]
+                const randomIndex2 = getRandomNumber(symbolArr)
+                password2 += symbolArr[randomIndex2]
+            } else if(numberEl.checked && !symbolEl.checked){
+                const numberArr = [...characters,...numbers]
+                const randomIndex1 = getRandomNumber(numberArr)
+                password1 += numberArr[randomIndex1]
+                const randomIndex2 = getRandomNumber(numberArr)
+                password2 += numberArr[randomIndex2]
+            } else if(numberEl.checked && symbolEl.checked){
+                const fullArr = [...characters,...symbols,...numbers]
+                const randomIndex1 = getRandomNumber(fullArr)
+                password1 += fullArr[randomIndex1]
+                const randomIndex2 = getRandomNumber(fullArr)
+                password2 += fullArr[randomIndex2]
+            } else {
+                const randomIndex1 = getRandomNumber(characters)
+                password1 += characters[randomIndex1]
+                const randomIndex2 = getRandomNumber(characters)
+                password2 += characters[randomIndex2]
+            }
         pass1.textContent = password1
         pass2.textContent = password2
     }
